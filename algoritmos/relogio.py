909d28f3729c9462cb43ff2c60b14d0962d84380
utheux <utheuz_ribeiro@hotmail.com>
2025-11-25 08:29:34 -0300

    paginacao_relogio

--- algoritmos/relogio.py
+++ algoritmos/relogio.py
@@ -1,5 +1,12 @@
-# --- ALGORITMO 3: RELÓGIO (quem?) ---
+# --- ALGORITMO 3: RELÓGIO (Matheus Ribeiro Silva) ---
 def algoritmo_relogio(matriz_ram, ponteiro_atual):
-   
-    print(f"ALGORITMO RELÓGIO AINDA NAO IMPLEMENTADO!")
-    return 0, 0 
+    while True:
+        pagina_atual = matriz_ram[ponteiro_atual]
+
+        if pagina_atual.R == 1:
+            pagina_atual.R = 0
+            ponteiro_atual = (ponteiro_atual + 1) % len(matriz_ram)
+        else:
+            vitima_idx = ponteiro_atual
+            proximo_ponteiro = (ponteiro_atual + 1) % len(matriz_ram)
+            return vitima_idx, proximo_ponteiro
